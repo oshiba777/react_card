9e574138674035bb321c3a8320b1cf0188a0a4f2
oshiba777 <stik_xi@mail.ru>
2026-04-29 10:51:32 +0500

    конец...

--- src/components/FormCreateCard.jsx
+++ src/components/FormCreateCard.jsx
@@ -1,16 +1,56 @@
 import './FormCreate.css';
-export function FormcreateCard(){
-    return(
-        <form className='form'>
+import React from 'react';
+import { useInput } from './UseInput.jsx';
+import { useForm } from 'react-hook-form';
+
+export function FormcreateCard() {
+
+    const [count, setCount] = React.useState(0);
+
+    const { register, handleSubmit, formState: { errors } }
+        = useForm({
+            defaultValues: {
+                fronttext: "",
+                backtext: ""
+            }
+        });
+    console.log(errors);
+
+    React.useEffect(() => {
+        if (count !== 0) {
+            console.log(`Отправка формы : ${count}`);
+            alert(`Отправка формы : ${count}`);
+        }
+    }, [count]);
+
+    return (
+        <form className='form' onSubmit={handleSubmit((data) => {
+            setCount(count + 1);
+            console.log(data);
+        })}>
             <label className='label'>
                 Передняя сторона карточки:
-                <input type="text" name="fronttext" className='input' />
+                <input type="text" name="fronttext" className='input' id='fronttext'  {...register("fronttext", {
+                    required: "Заполните поле название набора",
+                    minLength: {
+                        value: 4,
+                        message: 'Название набора должно содержать минимум 4 символа'
+                    }
+                })} />
+                <p className='error-msg'>{errors.fronttext?.message}</p>
             </label>
             <label className='label'>
                 Задняя сторона карточки:
-               <input type="text" name="backtext" className='input' />
+                <input type="text" name="backtext" className='input' id='backtext' {...register("backtext", {
+                    required: "Заполните поле название набора",
+                    minLength: {
+                        value: 4,
+                        message: 'Название набора должно содержать минимум 4 символа'
+                    }
+                })} />
+                <p className='error-msg'>{errors.backtext?.message}</p>
             </label>
-            <input type="submit" value="Создать карточку" className='create' /> 
+            <input type="submit" value="Создать карточку" className='create' />
         </form>
-    ); 
+    );
 }
